ad2a1f07c67111877e07d512881f5594bc6c0448
an.le6 <anle.drewindeed@gmail.com>
2023-10-01 16:03:14 +0700

    0. chg: minor styles and routes

---
+++ app/(landing)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const LandingPage = () => {
+  return (
+    <>
+      <p>Landing (Unprotected)</p>
+      <Button>Click Me</Button>
+    </>
+  );
+};
+
+export default LandingPage;
