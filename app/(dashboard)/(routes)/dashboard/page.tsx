f421adfc7c39101c5d55a696a16848841597135d
an.le6 <anle.drewindeed@gmail.com>
2023-10-04 14:24:06 +0700

    2. feat: nav bar raw set up

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
     <div>
       <p>DashboardPage (protected)</p>
-      <UserButton afterSignOutUrl="/" />
     </div>
   );
 };
