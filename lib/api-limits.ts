16595dabe125136463bd87177fdcd21aa77b1b2d
an.le6 <anle.drewindeed@gmail.com>
2023-10-15 20:49:42 +0700

    24. feat: set up db methods to work with api limits

---
+++ lib/api-limits.ts
@@ -0,0 +1,38 @@
+import { auth } from "@clerk/nextjs";
+import prismadb from "./prisma.db";
+import { MAX_FREE_COUNT } from "@/constants";
+
+// update user api limit in db
+export const increaseApiLimit = async () => {
+  const { userId } = auth();
+  if (!userId) return;
+  const userApiLimtRs = await prismadb.userApiLimit.findUnique({
+    where: {
+      userId,
+    },
+  });
+  // if user limit exists -> update the existing limit
+  if (userApiLimtRs) {
+    await prismadb.userApiLimit.update({
+      where: { userId },
+      data: { count: userApiLimtRs.count + 1 },
+    });
+  } else {
+    await prismadb.userApiLimit.create({
+      data: { userId, count: 1 },
+    });
+  }
+};
+
+export const checkValidApiLimit = async () => {
+  const { userId } = auth();
+  if (!userId) return;
+  const userApiLimtRs = await prismadb.userApiLimit.findUnique({
+    where: {
+      userId,
+    },
+  });
+  // if user has not set a limit or the limit is still valid
+  if (!userApiLimtRs || userApiLimtRs.count < MAX_FREE_COUNT) return true;
+  return false;
+};
